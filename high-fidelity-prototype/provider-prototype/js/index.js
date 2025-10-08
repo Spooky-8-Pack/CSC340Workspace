9cc1de3e096f54be17c9276474e547fcdbe35a1b
glogloria <theglocode@gmail.com>
2025-10-08 09:53:25 -0400

    Moved log in panel funcitonality to separate js file

--- high-fidelity-prototype/provider-prototype/js/index.js
+++ high-fidelity-prototype/provider-prototype/js/index.js
@@ -14,14 +14,6 @@ rotateBtn.addEventListener('click', () => {
     }, 200);
 });
 
-// View login panel when profile icon is clicked
-const profile = document.getElementById('profile');
-const loginPanel = document.getElementById('login-panel');
-
-profile.addEventListener('click', () => {
-    loginPanel.classList.toggle('show');
-});
-
 // function clickOutside(element, closeWindow) {
 //     document.addEventListener('click', (event) => {
 //         if (!element.contains(event.target) && event.target !== element) {
