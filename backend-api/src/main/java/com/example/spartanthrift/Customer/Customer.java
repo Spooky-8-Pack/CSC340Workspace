ce80bbf22e3962e2ac8ab6ae255a63bf0b271d05
Adrian Aldridge <agaldridge@uncg.edu>
2025-10-21 13:24:43 -0400

    added customer classes

---
+++ backend-api/src/main/java/com/example/spartanthrift/Customer/Customer.java
@@ -0,0 +1,87 @@
+package com.example.spartanthrift.Customer;
+
+import org.springframework.web.bind.annotation.CookieValue;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "customers")
+public class Customer {
+    //all fields for a customer should be filled as we want a customer to have every attribute
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long customerId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String address;
+
+    //blank constructor
+    public Customer(){
+
+    }
+
+    //constructor with args
+    public Customer(Long customerId, String name, String password, String address, String email){
+        this.customerId = customerId;
+        this.address = address;
+        this.email = email;
+        this.name = name;
+        this.password = password;
+    }
+
+    //getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+       public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+        
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+     public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
---
+++ backend-api/src/main/java/com/example/spartanthrift/Customer/CustomerService.java
@@ -0,0 +1,5 @@
+package com.example.spartanthrift.Customer;
+
+public class CustomerService {
+    
+}
