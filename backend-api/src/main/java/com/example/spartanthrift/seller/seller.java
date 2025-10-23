cd5468ce8b1eb6e286163174041e17443df66c40
glogloria <theglocode@gmail.com>
2025-10-23 11:01:08 -0400

    Renamed seller to Seller (to include capital S), added initial variable declarations

--- backend-api/src/main/java/com/example/spartanthrift/seller/seller.java
+++ backend-api/src/main/java/com/example/spartanthrift/seller/seller.java
@@ -1,5 +1,6 @@
 package com.example.spartanthrift.seller;
 
+import jakarta.persistence.*;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -7,8 +8,34 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "sellers") 
-public class seller {
+public class Seller {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long sellerId;
+    private Long id;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String firstName;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String lastName;
+
+
+    @Email
+    @NotBlank
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @NotBlank
+    @Column(nullable = false) 
+    private String password;
+    
+    public seller() {
+    }
+
+    public Seller(Long id) {
+        this.id = id;
+    }
 }
