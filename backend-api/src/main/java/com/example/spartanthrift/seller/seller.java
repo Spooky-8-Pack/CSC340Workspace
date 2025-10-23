1dcb44008a8fa6e81c260865ea7c69ef551d7d76
glogloria <theglocode@gmail.com>
2025-10-23 11:24:40 -0400

    Added messages for @NotBlank

--- backend-api/src/main/java/com/example/spartanthrift/seller/seller.java
+++ backend-api/src/main/java/com/example/spartanthrift/seller/seller.java
@@ -1,41 +1,39 @@
 package com.example.spartanthrift.seller;
 
 import jakarta.persistence.*;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Email;
 
 @Entity
 @Table(name = "sellers") 
 public class Seller {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @NotBlank
-    @Column(nullable = false)
-    private String firstName;
-
-    @NotBlank
-    @Column(nullable = false)
-    private String lastName;
-
-
-    @Email
-    @NotBlank
-    @Column(unique = true, nullable = false)
-    private String email;
-
-    @NotBlank
-    @Column(nullable = false) 
-    private String password;
-    
-    public seller() {
+    public Seller() {
     }
 
     public Seller(Long id) {
         this.id = id;
     }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "First name required")
+    @Column(nullable = false)
+    private String firstName;
+
+    @NotBlank(message = "Last name required")
+    @Column(nullable = false)
+    private String lastName;
+
+    @Email
+    @NotBlank(message = "Email required")
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @NotBlank(message = "Password required")
+    @Column(nullable = false) 
+    private String password;
+    
 }
