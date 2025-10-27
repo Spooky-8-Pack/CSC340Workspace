b5c4bc21d8d8f9ef30140407b2c610d9ae2471ec
Adrian Aldridge <agaldridge@uncg.edu>
2025-10-27 14:27:57 -0400

    made product classes and coded some

---
+++ backend-api/src/main/java/com/example/spartanthrift/Product/ProductController.java
@@ -0,0 +1,5 @@
+package com.example.spartanthrift.Product;
+
+public class ProductController {
+
+}
---
+++ backend-api/src/main/java/com/example/spartanthrift/Product/ProductService.java
@@ -0,0 +1,53 @@
+package com.example.spartanthrift.Product;
+
+import java.util.List;
+
+import jakarta.persistence.EntityNotFoundException;
+
+public class ProductService {
+    private ProductRepository productRepository;
+
+    //create a new product
+    public Product createProduct(Product product){
+        return productRepository.save(product);
+    }
+
+    //update product
+    public Product updateProduct(Long id, Product productDetails){
+        Product product = productRepository.findById(id)
+            .orElseThrow(() -> new EntityNotFoundException("Product Not Found"));
+
+        product.setName(productDetails.getName());
+        product.setDescription(productDetails.getDescription());
+        product.setPrice(productDetails.getPrice());
+        product.setAvailable(productDetails.isAvailable());
+
+        return productRepository.save(product);
+    }
+
+    //delete product
+    public void deleteProduct(Long id){
+        if(!productRepository.existsById(id)){
+            throw new EntityNotFoundException("Product Not Found");
+        }
+        productRepository.deleteById(id);
+    }
+
+    //get product by Id
+    public Product getProductById(Long id){
+        return productRepository.findById(id)
+            .orElseThrow(() -> new EntityNotFoundException("Product Not Found"));
+    }
+
+    //get all products
+    public List<Product> getAllProducts(){
+        return productRepository.findAll();
+    }
+
+    //get available products
+    public List<Product> getAvailableProducts(){
+        return productRepository.findByAvailable(true);
+    }
+
+    //get by seller goes here
+}
