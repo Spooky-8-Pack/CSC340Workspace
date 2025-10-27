b35ca3b2e6f7e8a1f02b604596bd262ac4caf457
Adrian Aldridge <agaldridge@uncg.edu>
2025-10-27 16:09:38 -0400

    finished cart controller

--- backend-api/src/main/java/com/example/spartanthrift/Cart/CartController.java
+++ backend-api/src/main/java/com/example/spartanthrift/Cart/CartController.java
@@ -1,8 +1,50 @@
 package com.example.spartanthrift.Cart;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.spartanthrift.Customer.CustomerService;
+import com.example.spartanthrift.Product.ProductService;
 
 @RestController
 public class CartController {
+    private CartService cartService;
+    private ProductService productService;
+    private CustomerService customerService;
+    //seller service goes here
 
+    @PostMapping
+    public ResponseEntity<Cart> createCart(@RequestBody Cart cart){
+        return ResponseEntity.ok(cartService.createCart(cart));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Cart> updateCart(@PathVariable Long id, @RequestBody Cart cart){
+        return ResponseEntity.ok(cartService.updateCart(id, cart));
+    }
+
+    @PostMapping("/{id}/cancel")
+    public ResponseEntity<Void> removeFromCart(@PathVariable Long id){
+        cartService.removeCart(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/customer/{customerId}")
+    public ResponseEntity<List<Cart>> getCustomerCart(@PathVariable Long customerId){
+        return ResponseEntity.ok(cartService.getCartByCustomer(customerService.getCustomerById(customerId)));
+    }
+
+    @GetMapping("/product/{productId}")
+    public ResponseEntity<List<Cart>> getCartItems(@PathVariable Long productId){
+        return ResponseEntity.ok(cartService.getCartByProduct(productService.getProductById(productId)));
+    }
+    
+    //get by seller goes here
 }
