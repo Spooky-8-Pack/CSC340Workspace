f5ae7dbaf78a0669990b20968789d6b6de04bc01
glogloria <theglocode@gmail.com>
2025-10-26 23:01:12 -0400

    Added seller variable columns

---
+++ backend-api/src/main/java/com/example/spartanthrift/seller/Seller.java
@@ -0,0 +1,54 @@
+package com.example.spartanthrift.seller;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "sellers")
+public class Seller {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String firstName;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String lastName;
+
+    @Email
+    @NotBlank
+    @Column(nullable = false)
+    private String email;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String password;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String shopName;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String shopDescription;
+
+    // Optional image for shop storefront
+    @Column
+    private String shopImagePath;
+    // Add code for image upload
+
+}
