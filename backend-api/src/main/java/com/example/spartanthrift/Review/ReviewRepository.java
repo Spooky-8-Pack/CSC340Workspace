a0fd45ae84b6ed2a8b6774ff1761c0e1f426a93f
Adrian Aldridge <agaldridge@uncg.edu>
2025-10-27 15:06:46 -0400

    coded review service and repo

--- backend-api/src/main/java/com/example/spartanthrift/Review/ReviewRepository.java
+++ backend-api/src/main/java/com/example/spartanthrift/Review/ReviewRepository.java
@@ -1,5 +1,16 @@
 package com.example.spartanthrift.Review;
 
-public interface ReviewRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.spartanthrift.Customer.Customer;
+import com.example.spartanthrift.Product.Product;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+    List<Review> findByProduct(Product product);
+    List<Review> findByCustomer(Customer customer);
+    //find by seller goes here
 }
