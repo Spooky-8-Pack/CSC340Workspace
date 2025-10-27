f3f68feffe6fa361efd0d3889286686d56f084be
glogloria <theglocode@gmail.com>
2025-10-26 23:01:47 -0400

    Added seller ocntroller with create, update, and get Seller methods

---
+++ backend-api/src/main/java/com/example/spartanthrift/seller/SellerController.java
@@ -0,0 +1,30 @@
+package com.example.spartanthrift.seller;
+
+import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/api/sellers")
+@RequiredArgsConstructor
+public class SellerController {
+    private final SellerService sellerService;
+
+    @PostMapping
+    public ResponseEntity<Seller> createSeller(@Valid @RequestBody Seller seller) {
+        return ResponseEntity.ok(sellerService.createSeller(seller));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Seller> updateSeller(@PathVariable Long id, @Valid @RequestBody Seller sellerDetails) {
+        return ResponseEntity.ok(sellerService.updateSeller(id, sellerDetails));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Seller> getSeller(@PathVariable Long id) { 
+        return ResponseEntity.ok(sellerService.getSellerById(id));
+    }
+}
