2376fb1a09e13f960061a5ba5d05448bcd1b50e0
Adrian Aldridge <agaldridge@uncg.edu>
2025-10-27 15:38:13 -0400

    coded cart class and repo

--- backend-api/src/main/java/com/example/spartanthrift/Cart/Cart.java
+++ backend-api/src/main/java/com/example/spartanthrift/Cart/Cart.java
@@ -1,8 +1,67 @@
 package com.example.spartanthrift.Cart;
 
+import java.time.LocalDateTime;
+
+import com.example.spartanthrift.Customer.Customer;
+import com.example.spartanthrift.Product.Product;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Cart {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id", nullable = false)
+    @JsonIgnoreProperties("cart")
+    private Customer customer;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    @JsonIgnoreProperties({"cart", "reviews"})
+    private Product product;
+
+    private LocalDateTime dateAdded;
+
+    //getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+        public LocalDateTime getDateAdded() {
+        return dateAdded;
+    }
+
+    public void setDateAdded(LocalDateTime dateAdded) {
+        this.dateAdded = dateAdded;
+    }
 }
--- backend-api/src/main/java/com/example/spartanthrift/Cart/CartRepository.java
+++ backend-api/src/main/java/com/example/spartanthrift/Cart/CartRepository.java
@@ -1,9 +1,17 @@
 package com.example.spartanthrift.Cart;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.spartanthrift.Customer.Customer;
+import com.example.spartanthrift.Product.Product;
+
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
+    List<Cart> findByCustomer(Customer customer);
+    List<Cart> findByProduct(Product product);
+    //find by seller goes here
 
 }
