c3714476464059414e44cf61b9451dd4b0251fec
Adrian Aldridge <agaldridge@uncg.edu>
2025-10-27 14:54:12 -0400

    finished review class

--- backend-api/src/main/java/com/example/spartanthrift/Review/Review.java
+++ backend-api/src/main/java/com/example/spartanthrift/Review/Review.java
@@ -1,5 +1,101 @@
 package com.example.spartanthrift.Review;
 
-public class Review {
+import java.time.LocalDateTime;
 
+import com.example.spartanthrift.Customer.Customer;
+import com.example.spartanthrift.Product.Product;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id", nullable = false)
+    @JsonIgnoreProperties({"reviews", "subscriptions"})
+    private Customer customer;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    @JsonIgnoreProperties("reviews")
+    private Product product;
+
+    private Double rating;
+
+    @Column(columnDefinition = "TEXT")
+    private String comment;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(columnDefinition = "TEXT")
+    private String sellerResponse;
+
+    //getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
+
+        public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getSellerResponse() {
+        return sellerResponse;
+    }
+
+    public void setSellerResponse(String sellerResponse) {
+        this.sellerResponse = sellerResponse;
+    }
+    
 }
