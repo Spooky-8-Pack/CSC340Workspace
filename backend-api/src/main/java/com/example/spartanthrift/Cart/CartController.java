508c4b1bd6f2343b77bf3c8254a0a3d9292e01e9
Adrian Aldridge <agaldridge@uncg.edu>
2025-10-27 23:11:20 -0400

    minor edits

--- backend-api/src/main/java/com/example/spartanthrift/Cart/CartController.java
+++ backend-api/src/main/java/com/example/spartanthrift/Cart/CartController.java
@@ -8,19 +8,21 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.spartanthrift.Customer.CustomerService;
 import com.example.spartanthrift.Product.ProductService;
 
 @RestController
+@RequestMapping("/cart")
 public class CartController {
     private CartService cartService;
     private ProductService productService;
     private CustomerService customerService;
     //seller service goes here
 
-    @PostMapping
+    @PostMapping("/create")
     public ResponseEntity<Cart> createCart(@RequestBody Cart cart){
         return ResponseEntity.ok(cartService.createCart(cart));
     }
