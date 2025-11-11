5c19623a7113f1fc6d52fbcc5716176dd8fbce35
glogloria <theglocode@gmail.com>
2025-11-10 22:14:27 -0500

    Changed findByShop to findBySeller

--- backend-api/src/main/java/com/example/spartanthrift/shop/ShopRepository.java
+++ backend-api/src/main/java/com/example/spartanthrift/shop/ShopRepository.java
@@ -5,8 +5,11 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.spartanthrift.seller.Seller;
+
 @Repository
 public interface ShopRepository extends JpaRepository<Shop, Long> {
-    Optional<Shop> findByShop(Shop shop);
+    Optional<Shop> findBySeller(Seller seller);
+
     boolean existsByShopName(String shopName);
 }
