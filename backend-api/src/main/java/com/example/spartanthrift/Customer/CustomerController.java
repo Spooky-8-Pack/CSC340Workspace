c431b38a79375f5ccf4cb00ffe9259acd08f8b82
Adrian Aldridge <agaldridge@uncg.edu>
2025-11-12 21:38:59 -0500

    started on endpoints

--- backend-api/src/main/java/com/example/spartanthrift/Customer/CustomerController.java
+++ backend-api/src/main/java/com/example/spartanthrift/Customer/CustomerController.java
@@ -10,22 +10,44 @@ public class CustomerController {
     @Autowired
     private CustomerService customerService;
 
-    @PostMapping("/customers")
-    public Object createCustomer(@RequestBody Customer customer) {
-        return customerService.createCustomer(customer);
+    
+    //get customer profile - view profile
+    @GetMapping("/customers/{id}")
+    public Object getCustomerById(@PathVariable Long id, Model model) {
+        //return customerService.getCustomerById(id);
+        model.addAttribute("customer", customerService.getCustomerById(id));
+        model.addAttribute("title", "Customer ID: ");
+        return "customer-profile";
+    }
+    
+    //get customer cart - view cart
+    @GetMapping("/customers/{id}/cart")
+    public Object getCustomerCart(@PathVariable Long id){
+        return "customer-cart";
     }
 
+    //create a customer - sign up
+    @PostMapping("/customer/sign up")
+    public Object createCustomer(Customer customer) {
+        //return customerService.createCustomer(customer);
+        Customer newCustomer = customerService.createCustomer(customer);
+        return "redirect:/home";
+    }
+
+    //update customer - update a profile
     @PutMapping("/customers/{id}")
     public Customer updateCustomer(@PathVariable Long id, @RequestBody Customer customerDetails) {
         return customerService.updateCustomer(id, customerDetails);
     }
 
-    //get customer profile
+    //delete profile
     @GetMapping("/customers/{id}")
-    public Customer getCustomerById(@PathVariable Long id) {
-        return customerService.getCustomerById(id);
+    public Object deleteCustomer(@PathVariable Long id) {
+        customerService.deleteCustomer(id);
+        return "redirect:/home";
     }
 
+    //end points im not sure i'll need
     @GetMapping("/customers")
     public Object getAllCustomers() {
         return customerService.getAllCustomers();
@@ -34,11 +56,5 @@ public class CustomerController {
     @GetMapping("/search/address")
     public Object searchByAddress(@RequestParam String key) {
         return customerService.searchByAddress(key);
-    }
-
-    @DeleteMapping("/{id}")
-    public Object deleteCustomer(@PathVariable Long id) {
-        customerService.deleteCustomer(id);
-        return customerService.getAllCustomers();
     }
 }
