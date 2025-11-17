687456d37d56175251c6d8892c89e19b99dfcb18
glogloria <theglocode@gmail.com>
2025-11-16 21:12:59 -0500

    Added shopImagePath field with getters and setters

--- backend-api/src/main/java/com/example/spartanthrift/shop/Shop.java
+++ backend-api/src/main/java/com/example/spartanthrift/shop/Shop.java
@@ -41,14 +41,17 @@ public class Shop {
     @NotBlank
     private String location;
 
+    private String shopImagePath;
+
     public Shop() {
     }
 
-    public Shop(Seller seller, String shopName, String description, String location) {
+    public Shop(Seller seller, String shopName, String description, String location, String shopImagePath) {
         this.seller = seller; 
         this.shopName = shopName;
         this.description = description;
         this.location = location;
+        this.shopImagePath = shopImagePath;
     }
 
     public void setShopName(String shopName) {
@@ -90,5 +93,13 @@ public class Shop {
     // @OneToMany(mappedBy = "shop", cascade = CascadeType.ALL)
     // @JsonIgnoreProperties("shop")
     // private List<Product> products = new ArrayList<>();
+
+    public String getShopImagePath() {
+        return shopImagePath;
+    }
+
+    public void setShopImagePath(String shopImagePath) {
+        this.shopImagePath = shopImagePath;
+    }
 
 }
