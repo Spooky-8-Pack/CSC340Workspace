a81c0ed51a2faff0a1e9d9c248ee9ae9a163fa2b
glogloria <theglocode@gmail.com>
2025-11-16 22:37:58 -0500

    Removed NotNull

--- backend-api/src/main/java/com/example/spartanthrift/Review/Review.java
+++ backend-api/src/main/java/com/example/spartanthrift/Review/Review.java
@@ -15,7 +15,6 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
 
 @Entity
 public class Review {
