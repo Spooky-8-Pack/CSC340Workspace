8bd6f8923dfa6b5e66f9fb842a84a676ab2df247
glogloria <theglocode@gmail.com>
2025-11-16 23:03:18 -0500

    Updated createShop method to attach product to a specific shop

--- backend-api/src/main/java/com/example/spartanthrift/Product/ProductService.java
+++ backend-api/src/main/java/com/example/spartanthrift/Product/ProductService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.spartanthrift.shop.Shop;
+import com.example.spartanthrift.shop.ShopRepository;
 
 import jakarta.persistence.EntityNotFoundException;
 
@@ -13,9 +14,24 @@ import jakarta.persistence.EntityNotFoundException;
 public class ProductService {
     @Autowired
     private ProductRepository productRepository;
+    private final ShopRepository shopRepository;
 
-    //create a new product
-    public Product createProduct(Product product){
+    public ProductService(ProductRepository productRepository, ShopRepository shopRepository) {
+        this.productRepository = productRepository;
+        this.shopRepository = shopRepository;
+    }
+
+    /**
+     * Add a new product and attach it to a shop
+     * 
+     * @param shopId
+     * @param product
+     * @return
+     */
+    public Product createProduct(Long shopId, Product product) {
+        Shop shop = shopRepository.findById(shopId).orElseThrow(() -> new IllegalStateException("Shop not found"));
+
+        product.setShop(shop);
         return productRepository.save(product);
     }
 
