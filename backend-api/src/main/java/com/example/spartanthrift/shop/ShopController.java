32f9e3d69e954f674eab94cc41b9d95a36a3b1b4
glogloria <theglocode@gmail.com>
2025-11-18 10:38:16 -0500

    Remvoed unused SellerService and corresponding imports

--- backend-api/src/main/java/com/example/spartanthrift/shop/ShopController.java
+++ backend-api/src/main/java/com/example/spartanthrift/shop/ShopController.java
@@ -1,19 +1,16 @@
 package com.example.spartanthrift.shop;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.example.spartanthrift.Seller.Seller;
-import com.example.spartanthrift.Seller.SellerService;
 
 @Controller
 @RequestMapping("/api/shops")
@@ -21,11 +18,9 @@ public class ShopController {
 
     @Autowired
     private final ShopService shopService;
-    private final SellerService sellerService;
 
-    public ShopController(ShopService shopService, SellerService sellerService) {
+    public ShopController(ShopService shopService) {
         this.shopService = shopService;
-        this.sellerService = sellerService;
     }
 
     /**
@@ -35,62 +30,43 @@ public class ShopController {
      * @param model
      * @return
      */
-    @GetMapping("/{id}")
+    @GetMapping("/{shopId}")
     public String getShop(@PathVariable Long id, Model model) {
         model.addAttribute("shop", shopService.getShopById(id));
         model.addAttribute("title", "Shop #: " + id);
         return "shop-details"; //Need to make this
     }
 
-    // Method to get all shops
-    // @GetMapping
-    // public ResponseEntity<List<Shop>> getAllShops() {
-    //     return ResponseEntity.ok(shopService.getAllShops());
-    // }
-
     /**
-     *  Create a shop associated with a seller
-     * 
-     * @param sellerId
-     * @param shop
-     * @param shopImage
-     * @return
-     */    
-    @PostMapping("/{sellerId}/shop")
-    public Object createShop(@RequestParam Long sellerId, 
-                                Shop shop, 
-                                @RequestParam("shopImage") MultipartFile shopImage) {
-        Seller seller = sellerService.getSellerById(sellerId);
-        shopService.createShop(seller, shop, shopImage);
-
-        return "redirect:/api/sellers/" + sellerId + "/shop";
-    }
-
-    /**
-     *  Method to show update form
+     *  Displays  shop update form 
      * 
      * @param id
      * @param model
      * @return
      */
-    @GetMapping("/sellers/updateForm/{id}")
-    public Object showUpdateForm(@PathVariable Long id, Model model) {
-        Seller seller = sellerService.getSellerById(id);
+    @GetMapping("/updateForm/{shopId}")
+    public Object showUpdateForm(@PathVariable Long shopId, Model model) {
+        Shop shop = shopService.getShopById(shopId);
+        Seller seller = shop.getSeller();
+
+        model.addAttribute("shop", shop);
         model.addAttribute("seller", seller);
-        model.addAttribute("title", "Update Shop: " + id);
+        model.addAttribute("title", "Update Shop: " + shopId);
         return "shop-update"; // shop-update.ftlh
     }
 
     /**
      * Endpoint to update a shop
+     * 
      * @param id
      * @param image
-     * @return
+     * @return      Redirects to the updated storefront
      */
-    @PutMapping("/{id}")
+    @PostMapping("/update/{id}") // Doesn't update the right fields yet
     public Object updateShop(@PathVariable Long id, Shop shop, @RequestParam("shopImage") MultipartFile shopImage) {
-        shopService.updateShop(id, shop, shopImage);           
-        return "redirect:/sellers/" + id;
+        shopService.updateShop(id, shop, shopImage);     
+          
+        return "redirect:/api/sellers/" + shop.getSeller().getSellerId() + "/storefront";
     }
 
     // @DeleteMapping("/{id}")
