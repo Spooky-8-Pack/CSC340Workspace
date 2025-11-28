364425697304f28a32ec920bdb4b839122d6f9dd
glogloria <theglocode@gmail.com>
2025-11-28 02:04:26 -0500

    Added @Table for complete table of users

--- backend-api/src/main/java/com/example/spartanthrift/User/User.java
+++ backend-api/src/main/java/com/example/spartanthrift/User/User.java
@@ -8,16 +8,23 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorColumn;
+import jakarta.persistence.DiscriminatorType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 @Entity
+@Table(name = "users")
+@Inheritance(strategy = InheritanceType.JOINED)
 public abstract class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -42,6 +49,10 @@ public abstract class User implements UserDetails {
      public enum Role {
         CUSTOMER,
         SELLER
+    }
+    
+    public Long getId() {
+        return id;
     }
 
     public String getName() {
